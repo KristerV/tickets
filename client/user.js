4456a79b3b89f0a729fc83cdc14be0c80062181e
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-01-07 00:28:52 +0200

    queue works

--- client/user.js
+++ client/user.js
@@ -1,11 +1,8 @@
-// Template.user.rendered = function() {
-// 	var sessionName = Session.get('session')
-// 	var session = Sessions.findOne(sessionName)
-// 	_.each(session.current.students, function(one, two, three){
-// 		console.log(one)
-// 		console.log(two)
-// 		console.log(three)
-// 	})
-	
-// 	Sessions.update(sessionName, {})
-// }
+Template.user.events({
+	'submit form[name="queue"]': function(e, tmpl) {
+		e.preventDefault()
+		var set = {}
+		set['current.students.' + Meteor.userId() + '.queue'] = TimeSync.serverTime()
+		Sessions.update(Session.get('session'), {$set: set})
+	}
+})
--- server/allow.js
+++ server/allow.js
@@ -1,5 +1,14 @@
 Sessions.allow({
 	insert: function (userId, doc) {
 		return !!userId;
+	},
+	update: function(userId, doc, field, modify) {
+		if (!userId)
+			return false
+
+		// If changing ones own data
+		console.log(modify)
+		if ('current.students.'+userId+'.queue' in modify['$set'])
+			return true
 	}
 });
