d96b57af17ca09fdb93647dd98f76544ae2d95ee
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-01-07 14:06:25 +0200

    admin butons

--- client/layout.js
+++ client/layout.js
@@ -1,3 +1,10 @@
+Template.layout.helpers({
+	isAdmin: function() {
+		var session = Sessions.findOne(Session.get('session'))
+		return Meteor.user() && session.admin == Meteor.userId()
+	}
+})
+
 Template.layout.events({
 	'click .btn[name="en"], click .btn[name="et"]': function(e, tmpl) {
 		var lang = e.currentTarget.name
